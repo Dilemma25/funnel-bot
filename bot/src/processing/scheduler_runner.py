96249960ec5ac3f312e5b5f677f899d02e6d8121
Dilemma25 <valtert90@mail.ru>
2026-02-04 04:19:55 +1000

    add normal queue with redis, add worker

--- bot/src/processing/scheduler_runner.py
+++ bot/src/processing/scheduler_runner.py
@@ -1,10 +1,7 @@
 import asyncio
 import logging
 
-from src.core import init_redis
 from src.core.database import init_db, close_db
-from src.processing.locks import RedisLock
-from src.core.config import config
 from src.processing.scheduler import Scheduler
 
 
@@ -16,34 +13,22 @@ logger = logging.getLogger(__name__)
 
 
 async def main():
-    redis_lock = RedisLock()
-    redis_lock.connect()
-
     logger.info("Scheduler: запуск")
     is_db_init = False
 
     try:
-        if not await redis_lock.acquire():
-            logger.info("Scheduler уже работает, пропускаю")
-            return
-
-
         await init_db()
         is_db_init = True
 
-        redis = init_redis()
-        scheduler = Scheduler(redis, config["REDIS_STREAM_KEY"])
+        scheduler = Scheduler()
 
         await scheduler.push_ready_tasks()
-
 
     except Exception as e:
         logger.error(f"Ошибка в Scheduler: {e}", exc_info=True)
 
     finally:
         logger.info("Scheduler: завершил обработку задач")
-        await redis_lock.release()
-        await redis_lock.close()
         if is_db_init:
             await close_db()
 
