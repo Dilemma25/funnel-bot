c6fdf4cfd44e32a0950f0f8647d4455661494716
Dilemma25 <valtert90@mail.ru>
2026-02-20 22:37:53 +1000

    [WIP]

--- bot/scripts/task_scheduler/entrypoint.sh
+++ bot/scripts/task_scheduler/entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
-echo '* * * * * cd /app && PYTHONPATH=/app /usr/local/bin/poetry run python src/processing/schedulers/task_scheduler/scheduler_runner.py' | crontab -
-exec cron -f
+printenv | grep -v "no_proxy" >> /etc/environment
+cron -f
